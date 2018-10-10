ebba7d395ead1cb8171f5d63aac95fb56f5c08f3
Soomin Park <smtion@gmail.com>
2018-10-10 23:06:43 +0900

    tmp

---
+++ models/delegations.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, type) => {
+  var Delegation = sequelize.define('delegations', {
+    id: {
+      type: type.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    type: type.INTEGER,
+    from: type.STRING,
+    to: type.STRING,
+    amount: type.DOUBLE,
+    created_at: type.DATETIME
+  })
+
+  return Delegation
+}
